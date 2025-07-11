9953fe095aa6eca03fc6746c77a972830e249953
Wilder.tabares@jala.university <wilder.tabares@jala.university>
2025-07-10 21:06:39 -0500

    Fix Gradle dependencies

---
+++ Mandarina/src/main/java/org/tomasa/mandarina/commands/PlayerVaultCommand.java
@@ -0,0 +1,12 @@
+package org.tomasa.mandarina.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class PlayerVaultCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@org.jetbrains.annotations.NotNull CommandSender sender, @org.jetbrains.annotations.NotNull Command command, @org.jetbrains.annotations.NotNull String label, @org.jetbrains.annotations.NotNull String @org.jetbrains.annotations.NotNull [] args) {
+        return false;
+    }
+}
