21900dfaec17ea339bc17ce4bb21535af3d0c411
Juanrojo1203 <juanrojo120303@gmail.com>
2025-07-13 16:05:57 -0500

    implement message utils

--- Mandarina/src/main/java/org/tomasa/mandarina/commands/deathCommand.java
+++ Mandarina/src/main/java/org/tomasa/mandarina/commands/deathCommand.java
@@ -1,6 +1,5 @@
 package org.tomasa.mandarina.commands;
 
-
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,46 +10,46 @@ import org.tomasa.mandarina.utils.messageutil;
 public class deathCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender Sender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] args) {
-        Player player =(Player) Sender;
-        if (args.length >= 1) {
-            //Mensaje de prueba
-            player.sendMessage(messageutil.getColoredMessage("pruebapruebaprueba"));
-            //Mensaje de que el getLastDeathLocation es null
-            if(player.getLastDeathLocation()==null){
-                player.sendMessage(messageutil.getColoredMessage("&cNo has muerto todavia"));
-
-            }
-
-
-            // /muerte cord
-            //comando de coordenadas de ultima muerte
-            else if(args[0].equalsIgnoreCase("cord")) {
-                //manda en el chat las ultimas coordenadas de muerte
-                player.sendMessage("Tus ultimas coordenadas de muerte son: " + "X=" + player.getLastDeathLocation().getBlockX() + " Y=" + player.getLastDeathLocation().getBlockY() + " Z=" + player.getLastDeathLocation().getBlockZ());
-            }
-
-
-            // /muerte tp
-            //comando de teleportacion hacia las ultimas coordenadas de muerte
-            else if(args[0].equalsIgnoreCase("tp")){
-                //teleporta al jugador a las ultimas coordenadas de muerte
-                player.teleport(player.getLastDeathLocation());
-            }
-
-
-            //muerte help
-            //conmando de ayuda
-            else if (args[0].equalsIgnoreCase("help")){
-            player.sendMessage(messageutil.getColoredMessage("&cComandos de muerte:"));
-            player.sendMessage(messageutil.getColoredMessage("&b - &6&l/muerte cord&r&c para ver tus ultimas coordenadas de muerte"));
-            player.sendMessage(messageutil.getColoredMessage("&b - &6&l/muerte tp&r&c para teleportarte a tus ultimas coordenadas de muerte"));
-
-
+        
+        if (!(Sender instanceof Player)) {
+            Sender.sendMessage(messageutil.getColoredMessage("&cEste comando solo puede ser usado por jugadores."));
             return true;
         }
 
-    return false;
+        Player player = (Player) Sender;
+
+
+        String subcommand = args[0];
+
+        if (player.getLastDeathLocation() == null) {
+            player.sendMessage(messageutil.getColoredMessage("&cNo has muerto todavía"));
+            return true;
+        }
+
+        switch (subcommand) {
+            case "cord":
+                player.sendMessage("Tus últimas coordenadas de muerte son: " + 
+                    "X=" + player.getLastDeathLocation().getBlockX() + 
+                    " Y=" + player.getLastDeathLocation().getBlockY() + 
+                    " Z=" + player.getLastDeathLocation().getBlockZ());
+                break;
+                
+            case "tp":
+                player.teleport(player.getLastDeathLocation());
+                player.sendMessage("Te has teletransportado a tus últimas coordenadas de muerte");
+                break;
+        
+            case "help":
+                player.sendMessage(messageutil.getColoredMessage("&cComandos de muerte:"));
+                player.sendMessage(messageutil.getColoredMessage("&b - &6&l/muerte cord&r&c para ver tus últimas coordenadas de muerte"));
+                player.sendMessage(messageutil.getColoredMessage("&b - &6&l/muerte tp&r&c para teleportarte a tus últimas coordenadas de muerte"));
+                break;
+
+            default:
+                player.sendMessage(messageutil.getColoredMessage("&cComando no reconocido. Usa /muerte help para ver los comandos disponibles."));
+                break;
+        }
+
+        return true;
     }
-        return false;
-}
 }
--- Mandarina/src/main/java/org/tomasa/mandarina/commands/sCommand.java
+++ Mandarina/src/main/java/org/tomasa/mandarina/commands/sCommand.java
@@ -3,6 +3,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
+import org.tomasa.mandarina.utils.messageutil;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
@@ -10,13 +11,13 @@ public class sCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if (!(sender instanceof Player)) {
-            sender.sendMessage("Este comando solo puede ser usado por jugadores.");
-            return true;
-        }
+        // if (!(sender instanceof Player)) {
+        //     sender.sendMessage("Este comando solo puede ser usado por jugadores.");
+        //     return true;
+        // }
 
         if (args.length == 0) {
-            sender.sendMessage("Uso: /s <jugador>");
+            sender.sendMessage(messageutil.getColoredMessage("&cEl comando tiene la siguiente sintaxis: /s <jugador>"));
             return true;
         }
 
@@ -24,22 +25,22 @@ public class sCommand implements CommandExecutor {
         Player target = Bukkit.getPlayer(args[0]);
 
         if (target == null) {
-            sender.sendMessage("El jugador no existe o está desconectado.");
+            sender.sendMessage(messageutil.getColoredMessage("&cEl jugador no existe o está desconectado."));
             return true;
         }else if (target == player) {
-            sender.sendMessage("No puedes traerte a ti mismo.");
+            sender.sendMessage(messageutil.getColoredMessage("&cNo puedes traerte a ti mismo."));
             return true;
         }else if (!target.isOnline()) {
-            sender.sendMessage("El jugador no está en línea.");
+            sender.sendMessage(messageutil.getColoredMessage("&cEl jugador no está en línea."));
             return true;
         }
         if (args.length == 1) {
-            sender.sendMessage("el jugador " + target.getName() + " ha sido teletransportado a ti.");
+            sender.sendMessage(messageutil.getColoredMessage("&fEl jugador &b&l" + target.getName() + "&r&f ha sido teletransportado a ti."));
             target.teleport(player.getLocation());
-            player.sendMessage("has sido teletransportado a " + target.getName() + ".");
+            player.sendMessage(messageutil.getColoredMessage("&fhas sido teletransportado a&b&l " + target.getName() + "&f&r."));
             return true;
         }else{
-            sender.sendMessage("El comando tiene la siguiente sintaxis: /s <jugador>");
+            sender.sendMessage(messageutil.getColoredMessage("&cEl comando tiene la siguiente sintaxis: /s <jugador>"));
         }
 
 
